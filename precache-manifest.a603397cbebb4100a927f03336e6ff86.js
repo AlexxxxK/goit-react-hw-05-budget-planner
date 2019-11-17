b5dbed4753e34bcda6ad73498644d36a2bb5b2db
AlexxxxK <oleksandr.kozyr@outlook.com>
2019-11-17 23:48:09 +0200

    Updates

---
+++ precache-manifest.a603397cbebb4100a927f03336e6ff86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70632df5fe43d46dbd98fe87fe6e248f",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "a844c6c2d423cf4508e2",
+    "url": "/goit-react-hw-05-budget-planner/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "f8f3604aaab3cd498e4a",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.951a195d.chunk.css"
+  },
+  {
+    "revision": "a844c6c2d423cf4508e2",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.c367ccc7.chunk.js"
+  },
+  {
+    "revision": "f8f3604aaab3cd498e4a",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.80757717.chunk.js"
+  },
+  {
+    "revision": "7b80aa84ff15644a64ee",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.eac1075a.js"
+  }
+]);
